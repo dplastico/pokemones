6a43598a2648738e8f406f3e9351685eee4e906b
catalina <catalina@blade.tubatuba.net>
2019-06-21 23:59:04 -0400

    login flow fixed, but we need a seed for the database

--- bruteforce/config.php
+++ bruteforce/config.php
@@ -1,12 +1,32 @@
 <?php
 //este PHP conecta a la DB hay que quizas despues eliminar el debug para no dar pistas
 session_start();
-$host = "localhost"; 
+$host = $_ENV['DB_1_PORT_3306_TCP_ADDR']; 
+// $host = "localhost";
 $user = "root"; 
 $password = "toor";
 $dbname = "bruteforce"; 
+
 $con = mysqli_connect($host, $user, $password,$dbname);
 // debug de la conexion
-if (!$con) {
- die("Connection failed: " . mysqli_connect_error());
+if (mysqli_connect_errno())
+  {
+  die("Failed to connect to MySQL: " . mysqli_connect_error());
+  }
+
+$checktable = mysqli_query($con, "SELECT 1 from users LIMIT 1");
+
+
+if($checktable == FALSE) 
+{
+
+    $createtable = mysqli_query($con, "CREATE TABLE IF NOT EXISTS users (
+    NICKNAME varchar(16) NOT NULL,
+    PASSWD varchar(16) NOT NULL)");
+
+    die (mysqli_error($con));
+    // $createtable;
+} else 
+{
+    // mysqli_error($con);
 }
--- bruteforce/login.php
+++ bruteforce/login.php
@@ -1,26 +1,32 @@
 <?php
-//aca llamo a la conexion a la db para que reciba los POST de username y password
 include "config.php";
+
 if(isset($_POST['but_submit'])){
     $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
     $password = mysqli_real_escape_string($con,$_POST['txt_pwd']);
+    
     if ($uname != "" && $password != ""){
-        //query de SQL a los pokemones, eso mismo malformaremos para la inyeccion
-        $sql_query = "select count(*) as cntUser from bruteforce where username='".$uname."' and password='".$password."'";
+
+        $sql_query = "select count(*) as cntUser from users where nickname='".$uname."' and passwd='".$password."'";
         $result = mysqli_query($con,$sql_query);
         $row = mysqli_fetch_array($result);
+
         $count = $row['cntUser'];
+
         if($count > 0){
             $_SESSION['uname'] = $uname;
             header('Location: home.php');
+            echo "welcome. ", mysqli_error($con);
+
         }else{
-            echo "Invalid username and password";
-        }
+            echo "Invalid username and password. ", mysqli_error($con);
+        }        
+    } else {
+        echo "U must specify user and password. ", mysqli_error($con);
     }
 }
 ?>
-<!-- creo que falta ponerle el estilo y eso ahi no se -->
-
+ <!-- //creo que falta ponerle el estilo y eso ahi no se -->
 <div class="container">
     <form method="post" action="">
         <div id="div_login">
