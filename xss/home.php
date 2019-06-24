cedc7e929e32180cd6ede193436719d12d53263e
cartulina <catalinapimentel@gmail.com>
2019-06-23 21:18:50 -0400

    contact form WIP

--- xss/home.php
+++ xss/home.php
@@ -1,31 +1,30 @@
 <?php
-include "config.php";
-// include "header.php";
+include "include/header.php";
+//declaracion variables de inputs del form
 
-// if(isset($_POST['but_submit'])){
-//     $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
-//     $password = mysqli_real_escape_string($con,$_POST['txt_pwd']);
-    
-//     if ($uname != "" && $password != ""){
+// $visitMail = "";
+// $visitSubject = "";
+// $visitMessage = "";
 
-//         $sql_query = "select count(*) as cntUser from users where nickname='".$uname."' and passwd='".$password."'";
-//         $result = mysqli_query($con,$sql_query);
-//         $row = mysqli_fetch_array($result);
+if(isset($_POST['but_submit'])) {
 
-//         $count = $row['cntUser'];
+    $visitMail = $_POST['txt_email'];
+    $visitSubject = $_POST['txt_subject'];
+    $visitMessage = $_POST['txt_message'];
 
-//         if($count > 0){
-//             $_SESSION['uname'] = $uname;
-//             header('Location: home.php');
-//             echo "welcome. ", mysqli_error($con);
+    if ($visitMail != "" && $visitSubject != "" && $visitMessage != ""){
 
-//         }else{
-//             echo "Invalid username and password. ", mysqli_error($con);
-//         }        
-//     } else {
-//         echo "U must specify user and password. ", mysqli_error($con);
-//     }
-// }
+        // $sql_query = "select count(*) as cntUser from users where nickname='".$uname."' and passwd='".$password."'";
+        // $result = mysqli_query($con,$sql_query);
+        // $row = mysqli_fetch_array($result);
+
+        // $count = $row['cntUser'];
+        echo "<p>este es tu email  $visitMail</p>";
+    } else {
+        echo " no hay datos";
+    }
+}
+
 ?>
  <!DOCTYPE html>
 <html lang="en">
@@ -37,18 +36,16 @@ include "config.php";
    </head>
    <body>
 <div class="container">
-    <!-- <//?php include("../include/header.php"); ?>   -->
-
     <div class="container-narrow">
     <div class="row">
 
     </div>
     <div class="col">
-    <form method="post" action="">
+    <form method="post" action="home.php">
         <div id="div_login">
             <h1>Déjanos un mensaje</h1>
             <div>
-                <input type="text" class="textbox" id="txt_email" name="txt-email" placeholder="email" />
+                <input type="text" class="textbox" id="txt_email" name="txt_email" placeholder="email" />
             </div>
             <div>
                 <input type="text" class="textbox" id="txt_subject" name="txt_subject" placeholder="asunto"/>
---
+++ xss/include/header.php
@@ -0,0 +1,3 @@
+<div id="div-header" class="row">
+    <a href="/" style="text-align:center"><img src="../img/pkmn-logo-big.png"></a>
+</div>
