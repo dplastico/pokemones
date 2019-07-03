5487883707a3dbf71591c170383d21c4f8ef5b6a
cartulina <catalinapimentel@gmail.com>
2019-07-03 09:01:49 -0400

    fix env

--- html/bruteforce/config.php
+++ html/bruteforce/config.php
@@ -1,10 +1,10 @@
 <?php
 //este PHP conecta a la DB hay que quizas despues eliminar el debug para no dar pistas
 session_start();
-$host = "localhost"; 
-$user = "root"; 
-$password = "toor";
-$dbname = "hola"; 
+$host = $_ENV['DB_PORT_3306_TCP_ADDR']; 
+$user = $_ENV['DB_ENV_MYSQL_USER']; 
+$password = $_ENV['DB_ENV_MYSQL_ROOT_PASSWORD'];
+$dbname = "bruteforce"; 
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 // debug de la conexion
