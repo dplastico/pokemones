91abc6ca6781b913858a76f7869a521239ce71a9
cartulina <catalinapimentel@gmail.com>
2019-07-03 08:42:40 -0400

    change for opt

---
+++ html/bruteforce/config.php
@@ -0,0 +1,14 @@
+<?php
+//este PHP conecta a la DB hay que quizas despues eliminar el debug para no dar pistas
+session_start();
+$host = "localhost"; 
+$user = "root"; 
+$password = "toor";
+$dbname = "hola"; 
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+// debug de la conexion
+if (!$con) {
+ die("Connection failed: " . mysqli_connect_error());
+}
+?>
